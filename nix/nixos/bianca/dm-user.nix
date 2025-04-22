3f90f2e202f62461a2c4134a566611dc69ef6a40
Antoine Sgambato <176003+Bambi@users.noreply.github.com>
2025-04-22 20:13:00 +0200

    Dm user on bianca managed with HM nixos module

--- nix/nixos/bianca/dm-user.nix
+++ nix/nixos/bianca/dm-user.nix
@@ -1,9 +1,20 @@
-_: {
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+{ inputs, ... }: {
+  imports = [
+    inputs.home-manager.nixosModules.home-manager
+  ];
+
   users.users.dm = {
     hashedPassword = "$y$j9T$gnbdTw.SyjFsl82aWGKzj0$yDoP35i/sMQ/iaKwla3IngMMLFKirPgCdVC9XL6E2TB";
     isNormalUser = true;
     description = "dm";
     extraGroups = [ "input" "wheel" "video" "audio" ];
+  };
+  home-manager = {
+    useGlobalPkgs = true;
+    useUserPackages = false;
+    users.dm = _: {
+      programs.bash.enable = true;
+      home.stateVersion = "24.11";
+    };
   };
 }
